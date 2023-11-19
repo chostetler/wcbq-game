17f3faffa706263e65b2f09602d2d6c471b00190
Caleb Hostetler <18chostetler@gmail.com>
2023-11-18 19:33:34 -0500

    Add models for simple game functionality

--- wcbqgame/quizgame/models.py
+++ wcbqgame/quizgame/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# Represents a specific bible version
+class BibleVersion(models.Model):
+    name = models.CharField(max_length=50)
+
+# Class represents a bible verse
+class BibleVerse(models.Model):
+    book = models.CharField(max_length=50)
+    chapter = models.PositiveIntegerField()
+    verse = models.PositiveIntegerField()
+    text = models.TextField()
+    version = models.ForeignKey(BibleVersion, on_delete=models.CASCADE, related_name='verses')
+
+    def __str__(self):
+        return f'{self.book} {self.chapter}:{self.verse}'
+
+# Represents a single question
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     answer_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
-    verse = models.ForeignKey(BibleVerse, on_delete=models.CASCADE, related_name='associated_verse')
+    verse = models.ForeignKey(BibleVerse, on_delete=models.CASCADE, related_name='questions')
 
     def __str__(self):
         if len(self.question_text)>100:
@@ -13,19 +30,35 @@ class Question(models.Model):
         else:
             return self.question_text
 
-class BibleVerse(models.Model):
-    book = models.CharField(max_length=50)
-    chapter = models.PositiveIntegerField()
-    verse = models.PositiveIntegerField()
-    text = models.TextField()
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.CharField(max_length=255)
+    # Returns true if >2/3 of keywords are found in the guess string
+    def matches_guess_string(self, guess: str):
+        keywords = set(re.findall(r'\b\w+\b', self.text.upper()))
+        guesswords = set(re.findall(r'\b\w+\b', guess.upper()))
+        matches = union(keywords, guesswords)
+        if len(keywords)*0.66 <= len(matches):
+            return len(guesswords) <= len(keywords) * 3
+        return false
 
     def __str__(self):
-        return f'{self.book} {self.chapter}:{self.verse}'
+        return self.text
+
+
+
+# Classes for game functionality
+# Represents one person guessing on a question
+
+class Player(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    color_hex = models.CharField(max_length=7)
 
 class Guess(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='associated_question')
     guess_text = models.CharField(max_length=255)
     guess_date = models.DateTimeField('time guessed')
+    
 
     def __str__(self):
         return self.guess_text
--- wcbqgame/wcbqgame/settings.py
+++ wcbqgame/wcbqgame/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quizgame.apps.QuizgameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
