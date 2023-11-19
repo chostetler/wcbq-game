cae6020a8b275c423a25b8c55210849b6db39ec1
Caleb Hostetler <18chostetler@gmail.com>
2023-11-18 23:12:58 -0500

    Re-add tests

--- wcbqgame/quizgame/models.py
+++ wcbqgame/quizgame/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 # Create your models here.
 
@@ -29,7 +30,7 @@ class Question(models.Model):
         else:
             return self.question_text
     def guess_string_correct(self, guess:str):
-        for answer in self.answers:
+        for answer in self.answers.all():
             if answer.matches_guess_string(guess):
                 return True
         return False
@@ -41,10 +42,10 @@ class Answer(models.Model):
     def matches_guess_string(self, guess: str):
         keywords = set(re.findall(r'\b\w+\b', self.text.upper()))
         guesswords = set(re.findall(r'\b\w+\b', guess.upper()))
-        matches = union(keywords, guesswords)
+        matches = keywords.intersection(guesswords)
         if len(keywords)*0.66 <= len(matches):
             return len(guesswords) <= len(keywords) * 3
-        return false
+        return False
 
     def __str__(self):
         return self.text
